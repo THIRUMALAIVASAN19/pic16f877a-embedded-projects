3b5f794ce09e017979ad028012ff4583e6016a8a
THIRUMALAIVASAN19 <kthirumalaivasan75@gmail.com>
2025-12-31 17:22:55 +0530

    Add files via upload

---
+++ pull_down.c
@@ -0,0 +1,50 @@
+/*
+ * File:   pull_up.c
+ * Author: Admin
+ *
+ * Created on December 31, 2025, 3:20 PM
+ */
+
+// CONFIG
+#pragma config FOSC = EXTRC     // Oscillator Selection bits (RC oscillator)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config BOREN = OFF      // Brown-out Reset Enable bit (BOR disabled)
+#pragma config LVP = OFF        // Low-Voltage (Single-Supply) In-Circuit Serial Programming Enable bit (RB3 is digital I/O, HV on MCLR must be used for programming)
+#pragma config CPD = OFF        // Data EEPROM Memory Code Protection bit (Data EEPROM code protection off)
+#pragma config WRT = OFF        // Flash Program Memory Write Enable bits (Write protection off; all program memory may be written to by EECON control)
+#pragma config CP = OFF         // Flash Program Memory Code Protection bit (Code protection off)
+
+// #pragma config statements should precede project file includes.
+// Use project enums instead of #define for ON and OFF.
+
+#include <xc.h>
+#define _XTAL_FREQ 6000000
+
+
+
+void main(void) {
+    TRISC=0xF0;// switch 1 to 4
+    TRISB=0x00; // led 1,2
+    PORTC=0x00; 
+    PORTB=0x00;
+    
+    unsigned char value=0;
+    value=PORTC;
+    switch(value){
+        case 0x80:
+            PORTB=0x02;// Rc1 is on  
+            break;
+        case 0x40:
+            PORTB=0x42; // RC1 and RC2 is on 
+            break;
+        case 0x20:
+            PORTB=0x40;// RC6 is on
+            break;
+        case 0x10:
+           PORTB=0x00;// RC1 and RC6 is off
+           break;
+         
+            
+    }
+}
