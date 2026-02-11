27b03de6c0c218d5078df9f8464dc608845b0836
THIRUMALAIVASAN K <kthirumalaivasan75@gmail.com>
2026-02-11 18:25:04 +0530

    Add files via upload

---
+++ 7segment.c
@@ -0,0 +1,40 @@
+/*
+ * File:   7segment.c
+ * Author: Admin
+ *
+ * Created on May 24, 2025, 11:01 AM
+ */
+
+// PIC16F877A Configuration Bit Settings
+
+// 'C' source line config statements
+
+// CONFIG
+#pragma config FOSC = EXTRC     // Oscillator Selection bits (RC oscillator)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config BOREN = OFF      // Brown-out Reset Enable bit (BOR disabled)
+#pragma config LVP = OFF        // Low-Voltage (Single-Supply) In-Circuit Serial Programming Enable bit (RB3 is digital I/O, HV on MCLR must be used for programming)
+#pragma config CPD = OFF        // Data EEPROM Memory Code Protection bit (Data EEPROM code protection off)
+#pragma config WRT = OFF        // Flash Program Memory Write Enable bits (Write protection off; all program memory may be written to by EECON control)
+#pragma config CP = OFF         // Flash Program Memory Code Protection bit (Code protection off)
+
+// #pragma config statements should precede project file includes.
+// Use project enums instead of #define for ON and OFF.
+
+#include <xc.h>
+#define _XTAL_FREQ 6000000
+unsigned char a[10]={0xC0,0xF9,0xA4,0xB0,0x99,0x92,0x82,0xF8,0x80,0x90};
+void main(void) {
+    TRISC=0x00;
+    PORTC=0xFF;
+    while(1)
+    {
+        for(int i=0;i<10;i++)
+        {
+            PORTC=a[i];
+            __delay_ms(500);
+        }
+    }
+        
+}
